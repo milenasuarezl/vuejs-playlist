3d7e9ec37beaf0407a051efc147e45808eff815c
Milena Suarez <milena.suarez.luna@gmail.com>
2020-10-07 16:36:04 -0500

    Events

--- app.js
+++ app.js
@@ -2,13 +2,26 @@ new Vue({
     el: '#vue-app',
     data: {
         name: 'Milena',
+        age: 29,
         job: 'Software Engineer',
         website: 'http://www.milenasuarez.com',
-        websiteTag: '<a href="http://www.milenasuarez.com">Milena Website</a>'
+        websiteTag: '<a href="http://www.milenasuarez.com">Milena Website</a>',
+        x: 0,
+        y: 0
     },
     methods: {
-        greet: function(time){
+        greet: function (time) {
             return `Good${time},${this.name}`;
+        },
+        add: function (inc) {
+            this.age += inc;
+        },
+        substract: function (dec) { 
+            this.age -= dec; 
+        },
+        updateXY: function(event) {
+            this.x = event.offsetX;
+            this.y = event.offsetY;
         }
     }
 });
