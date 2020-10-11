c43686dc065d450e6d1b451d8dc5f063ab6927bc
Milena Suarez <milena.suarez.luna@gmail.com>
2020-10-11 16:05:30 -0500

    Keyboard events

--- app.js
+++ app.js
@@ -5,14 +5,17 @@ new Vue({
         age: 29,
         job: 'Software Engineer',
         website: 'http://www.milenasuarez.com',
-        websiteTag: '<a href="http://www.milenasuarez.com">Milena Website</a>',
+        websiteTag: '<a href="http://www.milenasuarez.com">v-html translate a html string in html code</a>',
         x: 0,
         y: 0
     },
     methods: {
+        /** Data binding and directives Section */
         greet: function (time) {
-            return `Good${time},${this.name}`;
+            return `Good ${time}, ${this.name}`;
         },
+
+        /** Events Section */
         add: function (inc) {
             this.age += inc;
         },
@@ -22,6 +25,17 @@ new Vue({
         updateXY: function(event) {
             this.x = event.offsetX;
             this.y = event.offsetY;
+        },
+        click: function() {
+            alert('click me');
+        },
+
+        /** KeyBoard Events Section */
+        logName: function() {
+            console.log('your enter your name');
+        },
+        logAge: function() {
+            console.log('your enter your age');
         }
     }
 });
